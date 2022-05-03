8e66c8ab6eadf8f5619e7dc5f8889fc338248d11
David Coomer <dmcoomer@hotmail.com>
2022-05-03 12:13:19 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', (event) =>  {
+    document.getElementById('text').innerHTML
+    = 'This is really cool!';
+    const id= 'This is really cool'   
+    console.log('DOM fully loaded and parsed');
+})
+
+
+
+
+
+// function (event) {
+
+// }
+
+// (event) => {}
